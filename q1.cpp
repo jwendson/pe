9c0609f98b620896270acc0606e6ea62d0c35fdb
IFPB <ifpb@MDK00LB10-JP>
2019-08-30 21:25:15 -0300

    mudei q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -7,6 +7,7 @@ int main () {
 cout << "Oi Mundo!, Estou pronto para C++ \n" ;
 cout << "Hey, escrever std:: o tempo todo é chato d+ \n" ;
 cout << "Mas eu sei como evitar isso :) \n" ;
+cout << "eu adoro o git" ;
 
 
 return 0 ;
